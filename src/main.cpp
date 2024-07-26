a1e347542790d2ca71e1e1b9acc39463e0b0af11
Alexandros444 <alexmohr0404@gmx.de>
2024-07-26 21:30:05 +0200

    advertising

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,29 @@
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     Serial.print("Device found: ");
-    Serial.print(advertisedDevice.getName().c_str());
+    if (advertisedDevice.haveName()) {
+      Serial.print(advertisedDevice.getName().c_str());
+    } else {
+      Serial.print("Unknown device");
+    }
     Serial.print(" RSSI: ");
     Serial.println(advertisedDevice.getRSSI());
   }
 };
 
-
 void setup() {
   Serial.begin(115200);
-  BLEDevice::init("ESP-1");
+  BLEDevice::init("ESP32_MyDevice");
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(BLEUUID((uint16_t)0x180D));
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+  // Start scanning
   BLEScan* pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
