e92c6ad1337e6c1d3f15fae17846d6c13f0dad27
Alexandros444 <alexmohr0404@gmx.de>
2024-07-27 15:44:08 +0200

    test timing 433MHz module

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,94 @@
 #include "BLEDevice.h"
 #include <Arduino.h>
+#include "esp_clk.h"
+#include "soc/cpu.h"
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("Device found: ");
-    if (advertisedDevice.haveName()) {
-      Serial.print(advertisedDevice.getName().c_str());
-    } else {
-      Serial.print("Unknown device");
-    }
-    Serial.print(" RSSI: ");
-    Serial.println(advertisedDevice.getRSSI());
-  }
-};
+#define REC_PIN 26
+#define SND_PIN 25
+#define LED_PIN 27
+
+
+// ulong rec_time_stamp = 0;
+
+
+// uint32_t start_cycle_counts = 0;
+// uint32_t end_cycle_counts = 0;
+
+// bool has_send_ping = false;
+
+// void maybe_send_ping() {
+// 	static ulong snd_time_stamp = 0;
+// 	static ulong snd_wait_time = 1000 + (random() % 10000);
+
+// 	if (millis() < snd_time_stamp + snd_wait_time)
+// 		return;
+	
+// 	snd_wait_time = 1000 + (random() % 10000);
+// 	has_send_ping = true;
+
+// 	digitalWrite(SND_PIN, HIGH);
+// 	delay(500);
+// 	digitalWrite(SND_PIN, LOW);
+// 	RSR(CCOUNT, start_cycle_counts);
+// }
+
+// void loop() {
+// 	int rec = digitalRead(REC_PIN);
+// 	if (rec)
+// 		RSR(CCOUNT, end_cycle_counts);
+// 	else {
+// 		maybe_send_ping();
+// 		return;
+// 	}
+
+
+// 	Serial.println(rec);
+
+// 	printf("Diff: %llu nsec\n", ((uint64_t)(cycle_counts - cycle_counts_last)*1000000000) / clock_freq);
+// 	cycle_counts_last = cycle_counts;
+
+// 	digitalWrite(LED_PIN, rec);
+
+	
+// 	delay(wait_ms);
+
+
+	
+
+
+// }
+
 
 void setup() {
-  Serial.begin(115200);
-  BLEDevice::init("ESP32_MyDevice");
+	Serial.begin(115200);
 
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(BLEUUID((uint16_t)0x180D));
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
-  BLEDevice::startAdvertising();
+	Serial.printf("Current Clock Speed: %d", esp_clk_cpu_freq());
+	
+	pinMode(REC_PIN, INPUT);
+	pinMode(SND_PIN, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
 
-  // Start scanning
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(30);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+	int rec = digitalRead(REC_PIN);
+	if (rec) {
+		digitalWrite(LED_PIN, HIGH);
+		delay(500);
+		digitalWrite(LED_PIN, LOW);
+	}
+
+	static ulong snd_wait_time = 1000 + (random() % 10000);
+	static ulong snd_last_time = 0;
+	if (millis() < snd_last_time + snd_wait_time)
+		return;
+
+	snd_wait_time = 1000 + (random() % 10000);
+	snd_last_time = millis();
+
+	digitalWrite(SND_PIN, HIGH);
+	delay(500);
+	digitalWrite(SND_PIN, LOW);
+
 }
